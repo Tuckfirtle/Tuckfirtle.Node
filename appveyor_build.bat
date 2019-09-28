e4e7099149f0c8531aed8ce8b698381cc220b3eb
Yong Jian Ming <jianming1993@gmail.com>
2019-09-28 17:14:25 +0800

    Update

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -16,6 +16,12 @@ dotnet publish %project% /p:PublishSingleFile=true -c %configuration% -f %framew
 dotnet publish %project% /p:PublishSingleFile=true -c %configuration% -f %framework_core% -o %output%/net-core-win-x64 -r win-x64 --self-contained
 dotnet publish %project% /p:PublishSingleFile=true -c %configuration% -f %framework_core% -o %output%/net-core-win-x86 -r win-x86 --self-contained
 
+copy include\net-core-linux-arm\liboqs.so bin\Release\net-core-linux-arm\liboqs.so
+copy include\net-core-linux-x64\liboqs.so bin\Release\net-core-linux-x64\liboqs.so
+copy include\net-core-osx-x64\liboqs.dylib bin\Release\net-core-osx-x64\liboqs.dylib
+copy include\net-core-win-x64\liboqs.dll bin\Release\net-core-win-x64\liboqs.dll
+copy include\net-core-win-x86\liboqs.dll bin\Release\net-core-win-x86\liboqs.dll
+
 7z a %output%/net-core-linux-arm.tar %output%/net-core-linux-arm
 7z a %output%/net-core-linux-arm.tar.gz %output%/net-core-linux-arm.tar -mx=9
 
--- build.bat
+++ build.bat
@@ -11,4 +11,6 @@ SET output=./bin/Release
 
 dotnet publish %project% /p:PublishSingleFile=true -c %configuration% -f %framework_core% -o %output%/net-core-win-x64 -r win-x64 --self-contained
 
+copy include\net-core-win-x64\liboqs.dll bin\Release\net-core-win-x64\liboqs.dll
+
 ENDLOCAL
